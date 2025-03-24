66633d37edc162dc2a49ae26473518c9594f86f2
Isfireomat <dfdfgfgf666@gmail.com>
2025-03-24 11:44:07 +0300

    chore: add PEP

--- app/df_processor.py
+++ app/df_processor.py
@@ -17,9 +17,15 @@ class DFProcessor:
         return cls._instance
 
     @staticmethod
-    def _calculate_profit(data: pd.DataFrame, df_id: int, year: int, prev: int, last_available: int) -> float:
+    def _calculate_profit(
+        data: pd.DataFrame, df_id: int, year: int, 
+        prev: int, last_available: int) -> float:
         values = list(map(
-            lambda field_sign: np.float64(DFProcessor._get_value(data, df_id, year, field_sign[0], prev, last_available) * field_sign[1]),
+            lambda field_sign: np.float64(
+                DFProcessor._get_value(
+                    data, df_id, year, 
+                    field_sign[0], prev, last_available) * field_sign[1]
+                ),
             DFProcessor._calculate_fields.items()
         ))
         return np.nan if pd.isna(values[0]) else sum(values)
@@ -29,27 +35,44 @@ class DFProcessor:
         return abs(pd.to_datetime(data.loc[df_id, 'Дата регистрации']).year - year)
 
     @staticmethod
-    def _get_value(data: pd.DataFrame, df_id: int, year: int, field_name: str, prev: int, last_available: int) -> float:
-        columns = list(filter(lambda col: col in data.columns, list(map(lambda y: f"{y}, {field_name}", range(year - prev, year - last_available - prev - 1, -1)))))        
+    def _get_value(
+        data: pd.DataFrame, df_id: int, year: int, field_name: str, 
+        prev: int, last_available: int) -> float:
+        columns = list(
+                    filter(
+                        lambda col: col in data.columns, 
+                        list(map(lambda y: f"{y}, {field_name}", 
+                        range(year - prev, year - last_available - prev - 1, -1))
+                    )
+                )
+            )        
         row_values = data.loc[df_id, columns]
         valid_value = row_values.first_valid_index()
         return row_values[valid_value] if valid_value is not None else np.nan
 
 
-    def get_data(self, data: pd.DataFrame, applications: pd.DataFrame, queries: list[dict[str, any]]) -> pd.DataFrame:
+    def get_data(   
+            self, data: pd.DataFrame, applications: pd.DataFrame, 
+            queries: list[dict[str, any]]) -> pd.DataFrame:
         output = data[['_id']].copy()
         
         def process_query(query):
             field_name = query.get('field_name')
             prev, last_available = query.get('prev', 0), query.get('last_available', 0)
             methods = {
-                "Прибыль (убыток) от продажи, RUB": lambda row: self._calculate_profit(data, row['_id'], row['year'], prev, last_available),
-                "Возраст компании, years": lambda row: self._get_company_age(data, row['_id'], row['year']),
-                "": lambda row: self._get_value(data, row['_id'], row['year'], field_name, prev, last_available)
+                "Прибыль (убыток) от продажи, RUB": 
+                    lambda row: self._calculate_profit(data, row['_id'], 
+                                                    row['year'], prev, last_available),
+                "Возраст компании, years": 
+                    lambda row: self._get_company_age(data, row['_id'], row['year']),
+                "": 
+                    lambda row: self._get_value(data, row['_id'], 
+                                                row['year'], field_name, prev, last_available)
             }
             method = methods.get(field_name, methods[""])
             
-            postfix = f"{('Prev' if prev > 0 else 'Next') * abs(prev)} {'LA' + str(last_available) if last_available else ''}".strip()
+            postfix = (f"{('Prev' if prev > 0 else 'Next') * abs(prev)}" 
+                       f" {'LA' + str(last_available) if last_available else ''}").strip()
             column_name = f"{field_name} {postfix}".strip()
             
             output[column_name] = applications.apply(method, axis=1)
--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ import numpy as np # import for eval
 from df_processor import DFProcessor
 from binary_files import applications_64, data_64, output_64
 from queries import query
-import time
 
 def to_dataframe(binary_file: bytes) -> DataFrame:
     return DataFrame(eval(base64.b64decode(binary_file).decode().replace("np.NaN", "np.nan")))
@@ -16,10 +15,8 @@ data: DataFrame = to_dataframe(data_64)
 output: DataFrame = to_dataframe(output_64)
 
 processor: DFProcessor = DFProcessor()
-start = time.time()
-new_output: DataFrame = processor.get_data(data, applications, query)
-end = time.time()
-# print(output.isna() == new_output.isna())
-# print(output)
-# print(new_output)
-print(end-start)
+new_output: DataFrame = processor.get_data(data.head(), applications.head(), query)
+
+print(output.isna() == new_output.isna())
+print(output)
+print(new_output)
